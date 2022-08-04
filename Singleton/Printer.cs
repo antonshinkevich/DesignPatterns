12fa01a64aaa092fe9d9d573cfe033f67d622bd2
antonshinkevich <antons.hinkevich@gmail.com>
2022-08-04 13:14:27 +0300

    Add class with multiton pattern

---
+++ Singleton/Printer.cs
@@ -0,0 +1,28 @@
+﻿namespace Singleton;
+
+public enum Subsystem
+{
+    Main, Backup
+}
+
+public class Printer
+{
+    private Printer()
+    {
+
+    }
+
+    public static Printer Get(Subsystem ss)
+    {
+        if (instances.ContainsKey(ss))
+        {
+            return instances[ss];
+        }
+
+        var instance = new Printer();
+        instances[ss] = instance;
+        return instance;
+    }
+
+    private static readonly Dictionary<Subsystem, Printer> instances = new Dictionary<Subsystem, Printer>();
+}
--- Singleton/Program.cs
+++ Singleton/Program.cs
@@ -3,3 +3,9 @@
 var db = SingletonDatabase.Instance;
 var city = "Tokyo";
 Console.WriteLine($"{city} has population {db.GetPopulation(city)}");
+
+// Multiton Pattern
+var primary = Printer.Get(Subsystem.Main);
+var backup = Printer.Get(Subsystem.Backup);
+var primaty2 = Printer.Get(Subsystem.Main);
+Console.WriteLine(ReferenceEquals(primary, primaty2));
