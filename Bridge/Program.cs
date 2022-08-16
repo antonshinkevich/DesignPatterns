80784961e8e739b6b5932ef2eaeb12a7aa103679
antonshinkevich <antons.hinkevich@gmail.com>
2022-08-16 18:18:44 +0300

    Add example

---
+++ Bridge/Program.cs
@@ -0,0 +1,26 @@
+﻿using Autofac;
+
+// 1st example
+var raster = new RasterRenderer();
+var vector = new VectorRenderer();
+var circle = new Circle(raster, 5);
+circle.Draw();
+circle.Resize(3);
+circle.Draw();
+
+// with dependency injection
+var cb = new ContainerBuilder();
+cb.RegisterType<VectorRenderer>().As<IRenderer>();
+cb.Register((c, p) => new Circle(
+    c.Resolve<IRenderer>(),
+    p.Positional<float>(0)));
+
+using (var c = cb.Build())
+{
+    var circle2 = c.Resolve<Circle>(
+        new PositionalParameter(0, 5.0f)
+        );
+    circle2.Draw();
+    circle2.Resize(3);
+    circle2.Draw();
+}
---
+++ Bridge/Shape.cs
@@ -0,0 +1,54 @@
+﻿public interface IRenderer
+{
+    void RenderCircle(float radius);
+}
+
+public class VectorRenderer : IRenderer
+{
+    public void RenderCircle(float radius)
+    {
+        Console.WriteLine($"Drawing circle of radius {radius} by {nameof(VectorRenderer)}");
+    }
+}
+
+public class RasterRenderer : IRenderer
+{
+    public void RenderCircle(float radius)
+    {
+        Console.WriteLine($"Drawing pixels for circle of radius {radius} by {nameof(RasterRenderer)}");
+    }
+}
+
+public abstract class Shape
+{
+    protected IRenderer renderer;
+
+    protected Shape(IRenderer renderer)
+    {
+        this.renderer = renderer;
+    }
+
+    public abstract void Draw();
+    public abstract void Resize(float factor);
+}
+
+public class Circle : Shape
+{
+
+    private float radius;
+
+    public Circle(IRenderer renderer, float radius) : base(renderer)
+    {
+        this.radius = radius;
+    }
+
+    public override void Draw()
+    {
+        renderer.RenderCircle(radius);
+    }
+
+    public override void Resize(float factor)
+    {
+        radius *= factor;
+    }
+}
