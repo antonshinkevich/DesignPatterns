eecb5496ed11c0622ea4231a771787b9390e344e
antonshinkevich <antons.hinkevich@gmail.com>
2022-08-17 13:49:02 +0300

    First example for Composite pattern.

---
+++ Composite/GraphicObject.cs
@@ -0,0 +1,42 @@
+﻿using System.Text;
+
+namespace Composite;
+
+public class GraphicObject
+{
+    public virtual string Name { get; set; } = "Group";
+    public string Color;
+
+    private readonly Lazy<List<GraphicObject>> _children = new Lazy<List<GraphicObject>>(
+        () => new List<GraphicObject>()
+    );
+    public List<GraphicObject> Children => _children.Value;
+
+    public override string ToString()
+    {
+        var sb = new StringBuilder();
+        Print(sb, 0);
+        return sb.ToString();
+    }
+
+    private void Print(StringBuilder sb, int depth)
+    {
+        sb.Append(new string('*', depth))
+            .Append(string.IsNullOrWhiteSpace(Color) ? string.Empty : $"{Color} ")
+            .AppendLine($"{Name}");
+        foreach (var child in Children)
+        {
+            child.Print(sb, depth + 1);
+        }
+    }
+}
+
+public class Circle : GraphicObject
+{
+    public override string Name => "Circle";
+}
+
+public class Square : GraphicObject
+{
+    public override string Name => "Circle";
+}
---
+++ Composite/Program.cs
@@ -0,0 +1,16 @@
+﻿// 1st example
+
+using Composite;
+
+var drawing = new GraphicObject
+{
+    Name = "My drawing"
+};
+
+drawing.Children.Add(new Square{ Color = "Red" });
+drawing.Children.Add(new Square { Color = "Yellow" });
+var group = new GraphicObject();
+group.Children.Add(new Square { Color = "Blue" });
+group.Children.Add(new Square { Color = "Blue" });
+drawing.Children.Add(group);
+Console.WriteLine(drawing);
