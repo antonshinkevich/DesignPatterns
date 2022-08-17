d1f84ad36324f0b2321635d4256327add8088334
antonshinkevich <antons.hinkevich@gmail.com>
2022-08-17 20:47:45 +0300

    Add 2 examples for Decorator

---
+++ Decorator/Program.cs
@@ -0,0 +1,16 @@
+﻿using Decorator;
+
+// 1st example
+var cb = new CodeBuilder();
+cb.AppendLine("class Foo")
+    .AppendLine("{")
+    .AppendLine("}");
+Console.WriteLine(cb);
+
+// 2nd example
+var circle = new Circle(2);
+Console.WriteLine(circle.AsString);
+var redCircle = new ColorShape(circle, "red");
+Console.WriteLine(redCircle.AsString);
+var redTransparentCircle = new TransparentShape(redCircle, 0.5f);
+Console.WriteLine(redTransparentCircle.AsString);
---
+++ Decorator/Shape.cs
@@ -0,0 +1,54 @@
+﻿public abstract class Shape
+{
+    public virtual string AsString => string.Empty;
+}
+
+public sealed class Circle : Shape
+{
+    private float radius;
+
+    public Circle()
+    {
+        
+    }
+    public Circle(float radius)
+    {
+        this.radius = radius;
+    }
+
+    public void Resize(float factor)
+    {
+        radius *= factor;
+    }
+
+    public override string AsString => $"A circle of radius {radius}";
+}
+
+public class ColorShape : Shape
+{
+    private Shape shape;
+    private string color;
+
+    public ColorShape(Shape shape, string color)
+    {
+        this.shape = shape;
+        this.color = color;
+    }
+
+    public override string AsString => $"{shape.AsString} has the color {color}";
+}
+
+public class TransparentShape : Shape
+{
+    private Shape shape;
+    private float transparency;
+
+    public TransparentShape(Shape shape, float transparency)
+    {
+        this.shape = shape;
+        this.transparency = transparency;
+    }
+
+    public override string AsString => $"{shape.AsString} has " +
+                                       $"{transparency * 100.0f}% transparency";
+}
