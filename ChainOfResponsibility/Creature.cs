b29341050209fe1aae6facb861909a211b95ab79
antonshinkevich <antons.hinkevich@gmail.com>
2022-08-31 10:57:38 +0300

    Add first example for Chain Of Responsibility pattern.

---
+++ ChainOfResponsibility/Creature.cs
@@ -0,0 +1,87 @@
+﻿namespace ChainOfResponsibility;
+
+public class Creature
+{
+    public string Name;
+    public int Attack, Defense;
+
+    public Creature(string name, int attack, int defense)
+    {
+        Name = name;
+        Attack = attack;
+        Defense = defense;
+    }
+
+    public override string ToString()
+    {
+        return $"{nameof(Name)}: {Name}, {nameof(Attack)}: {Attack}, {nameof(Defense)}: {Defense}";
+    }
+}
+
+public class CreatureModifier
+{
+    protected Creature creature;
+    protected CreatureModifier next; // singly linked list
+
+    public CreatureModifier(Creature creature)
+    {
+        this.creature = creature;
+    }
+
+    public void Add(CreatureModifier m)
+    {
+        if (next != null)
+        {
+            next.Add(m);
+        }
+        else
+        {
+            next = m;
+        }
+    }
+
+    public virtual void Handle() => next?.Handle();
+}
+
+class DoubleAttackModifier : CreatureModifier
+{
+    public DoubleAttackModifier(Creature creature) : base(creature)
+    {
+    }
+
+    public override void Handle()
+    {
+        Console.WriteLine($"Doubling {creature.Name}'s attack");
+        creature.Attack *= 2;
+        base.Handle();
+    }
+}
+
+class IncreaseDefenseModifier : CreatureModifier
+{
+    public IncreaseDefenseModifier(Creature creature) : base(creature)
+    {
+    }
+
+    public override void Handle()
+    {
+        if (creature.Attack <= 2)
+        {
+            Console.WriteLine($"Increasing {creature.Name}'s defense");
+            creature.Defense++;
+        }
+
+        base.Handle();
+    }
+}
+
+class NoBonusesModifier : CreatureModifier
+{
+    public NoBonusesModifier(Creature creature) : base(creature)
+    {
+    }
+
+    public override void Handle()
+    {
+    }
+}
---
+++ ChainOfResponsibility/Program.cs
@@ -0,0 +1,9 @@
+﻿using ChainOfResponsibility;
+
+var goblin  = new Creature("Goblin", 1, 1);
+Console.WriteLine(goblin);
+var root = new CreatureModifier(goblin);
+root.Add(new DoubleAttackModifier(goblin));
+root.Add(new DoubleAttackModifier(goblin));
+root.Handle();
+Console.WriteLine(goblin);
