969cbd8bf22ff11020cd1d6d1936d8632d3425c1
antonshinkevich <antons.hinkevich@gmail.com>
2022-09-04 19:37:35 +0300

    Simple example with Observer pattern

---
+++ Observer/Person.cs
@@ -0,0 +1,19 @@
+﻿namespace Observer;
+
+public class Person
+{
+    public event EventHandler<FallsIllEventArgs> FallsIll;
+
+    public void CatchACold()
+    {
+        FallsIll?.Invoke(this, new FallsIllEventArgs
+        {
+            Address = "123 London Road"
+        });
+    }
+}
+
+public class FallsIllEventArgs : EventArgs
+{
+    public string Address;
+}
---
+++ Observer/Program.cs
@@ -0,0 +1,12 @@
+﻿using Observer;
+
+// First example
+var person = new Person();
+person.FallsIll += PersonFallsIll;
+person.CatchACold();
+person.FallsIll -= PersonFallsIll;
+
+static void PersonFallsIll(object? sender, FallsIllEventArgs e)
+{
+    Console.WriteLine($"Call a doctor to {e.Address}");
+}
