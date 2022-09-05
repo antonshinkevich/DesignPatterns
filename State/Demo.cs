fb7f7ae93909ce2b91a71af752415e4c37e124f3
antonshinkevich <antons.hinkevich@gmail.com>
2022-09-05 20:40:19 +0300

    Example for State pattern

---
+++ State/Demo.cs
@@ -0,0 +1,45 @@
+﻿public enum State
+{
+    OffHook,
+    Connecting,
+    Connected,
+    OnHold,
+    OnHook
+}
+
+public enum Trigger
+{
+    CallDialed,
+    HungUp,
+    CallConnected,
+    PlacedOnHold,
+    TakenOffHold,
+    LeftMessage
+}
+
+public class Demo
+{
+    public static Dictionary<State, List<(Trigger, State)>> Rules { get; set; } = new()
+        {
+        [State.OffHook] = new List<(Trigger, State)>
+        {
+            (Trigger.CallDialed, State.Connecting)
+        },
+        [State.Connecting] = new List<(Trigger, State)>
+        {
+            (Trigger.HungUp, State.OnHook),
+            (Trigger.CallConnected, State.Connected)
+        },
+        [State.Connected] = new List<(Trigger, State)>
+        {
+            (Trigger.LeftMessage, State.OnHook),
+            (Trigger.HungUp, State.OnHook),
+            (Trigger.PlacedOnHold, State.OnHold)
+        },
+        [State.OnHold] = new List<(Trigger, State)>
+        {
+            (Trigger.TakenOffHold, State.Connected),
+            (Trigger.HungUp, State.OnHook)
+        }
+    };
+}
---
+++ State/Program.cs
@@ -0,0 +1,22 @@
+﻿State state = State.OffHook, exitState = State.OnHook;
+var queue = new Queue<int>(new []{0,1,2,0,0});
+
+do
+{
+    Console.WriteLine($"The phone is currently {state}");
+    Console.WriteLine("Select a trigger:");
+
+    for (int i = 0; i < Demo.Rules[state].Count; ++i)
+    {
+        var (t, _) = Demo.Rules[state][i];
+        Console.WriteLine($"{i}. {t}");
+    }
+
+    var input = queue.Dequeue();
+    Console.WriteLine(input);
+
+    var (_, s) = Demo.Rules[state][input];
+    state = s;
+} while (state != exitState);
+
+Console.WriteLine("We are done using the phone");
